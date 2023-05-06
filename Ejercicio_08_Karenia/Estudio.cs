ae9fe1e9f4065b466aa7baa80aad6fa032b5f681
Erwing Hernandez <birc89@hotmail.com>
2023-05-05 21:27:41 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio_08_Karenia/Estudio.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_08_Karenia
+{
+    class Estudio
+    {
+        string nom;
+        string des;
+
+        public Estudio(string nom, string des)
+        {
+            this.Nom = nom;
+            this.Des = des;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Des { get => des; set => des = value; }
+    }
+}
---
+++ Ejercicio_08_Karenia/Pelicula.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_08_Karenia
+{
+    class Pelicula
+    {
+        string nom;
+        int annio;
+
+        public Pelicula(string nom, int annio)
+        {
+            this.Nom = nom;
+            this.Annio = annio;
+        }
+
+        public string Nom { get => nom; set => nom = value; }
+        public int Annio { get => annio; set => annio = value; }
+    }
+}
